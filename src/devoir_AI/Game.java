66b15dbf96bfcd2299a8277994af6eeab0bc7268
khorgn <hugo.dupuyroudel@laposte.net>
2015-10-06 18:24:54 +0200

    methodes implementées quand possibles

--- src/devoir_AI/Game.java
+++ src/devoir_AI/Game.java
@@ -4,4 +4,40 @@ import model.*;
 
 public class Game {
 	
+	/**
+	 * renvoit un plateau modifié après le déplacement du joueur
+	 * @param p
+	 * @param m
+	 * @return
+	 */
+	public GameBoard play (Player p, Direction m){
+		return null;
+	}
+	
+	/**
+	 * renvoit vrai si le jeu est terminé, faux sinon (plateau en état terminal)
+	 * @return
+	 */
+	public boolean terminal(){
+		return false;
+	}
+	
+	/**
+	 * liste des états du plateau sur lequel peut arriver le joueur
+	 * @param p le joueur
+	 * @return
+	 */
+	public GameBoard[] next_move(Player p){
+		
+		return null;
+	}
+
+	/**
+	 * renvoit la valeur d'un plateau selon le premier joueur
+	 * @param g le plateau à étudier
+	 * @return la valeur du plateau
+	 */
+	public int value(GameBoard g){
+		return 0;
+	}
 }
